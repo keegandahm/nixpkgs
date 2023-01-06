28e413278a7bb2c10053188a579d7ea2ef0bf3c1
Keegan Dahm <kdahm@anduril.com>
2023-01-06 15:13:50 -0800

    protobuf: fix uses of .override

--- pkgs/development/libraries/protobuf/3.21.nix
+++ pkgs/development/libraries/protobuf/3.21.nix
@@ -1,6 +1,6 @@
-{ callPackage, abseil-cpp, ... }:
+{ callPackage, ... } @ args:
 
-callPackage ./generic-v3-cmake.nix {
+callPackage ./generic-v3-cmake.nix (args // {
   version = "3.21.8";
   sha256 = "sha256-cSNHX18CvMmydpYWqfe6WWk9rGxIlFfY/85rfSyznU4=";
-}
+})
